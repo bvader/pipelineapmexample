ef0c22509c989790ea1fc4474b57e66a7df4f6bc
bvader <stephen.brown@elastic.co>
2020-03-25 13:30:53 -0700

    Update Agent Version

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-AGENT_VERSION=1.13.0
+AGENT_VERSION=1.14.0
 curl -O https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/${AGENT_VERSION}/elastic-apm-agent-${AGENT_VERSION}.jar
 ./gradlew clean assemble
 ./run-processor.sh 1 Source 7070,7071,7072 &

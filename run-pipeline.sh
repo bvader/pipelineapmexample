145669759a55114c670bcee996176b3a5625f28a
bvader <stephen.brown@elastic.co>
2020-03-25 14:21:35 -0700

    Fix Version

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 AGENT_VERSION=1.14.0
 curl -O https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/${AGENT_VERSION}/elastic-apm-agent-${AGENT_VERSION}.jar
-./gradlew clean assemble
 ./run-processor.sh 1 Source 7070,7071,7072 &
 ./run-processor.sh 2 Process1 7070,7071,7072 &
 ./run-processor.sh 3 Process2 7070,7071,7072 &

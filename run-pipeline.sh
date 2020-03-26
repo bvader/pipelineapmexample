bd902c071038d4c01abb6b78e08d569bcf0e9d60
bvader <stephen.brown@elastic.co>
2020-03-26 10:14:00 -0700

    add build

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 AGENT_VERSION=1.14.0
 curl -O https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/${AGENT_VERSION}/elastic-apm-agent-${AGENT_VERSION}.jar
+./gradlew clean build
 ./run-processor.sh 1 Source 7070,7071,7072 &
 ./run-processor.sh 2 Process1 7070,7071,7072 &
 ./run-processor.sh 3 Process2 7070,7071,7072 &
